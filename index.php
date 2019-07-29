0c4857c6a9e9490fdebf47a7fe9fb72b99a6c8a4
kodokok <sabdo.tri.atmojo@gmail.com>
2019-07-29 19:15:29 +0700

    testing slim route

--- index.php
+++ index.php
@@ -2,3 +2,14 @@
 
 require 'vendor/autoload.php';
 
+$app = new \Slim\App;
+
+$app->get('/', function() {
+    echo 'Home';
+});
+
+$app->get('/users', function() {
+    echo 'Users';
+});
+
+$app->run();
