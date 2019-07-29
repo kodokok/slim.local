2a55652a66c373ae7e1ce343fce50bf1975dc3e8
kodokok <sabdo.tri.atmojo@gmail.com>
2019-07-29 22:23:29 +0700

    post routes test

--- index.php
+++ index.php
@@ -2,14 +2,18 @@
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\App;
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+    ]
+]);
 
 $container = $app->getContainer();
 
 // Register component on container
 $container['view'] = function ($container) {
     $view = new \Slim\Views\Twig(__DIR__ . '/resources/views', [
-        'cache' => false
+        'cache' => false,
     ]);
 
     // Instantiate and add Slim specific extension
@@ -20,20 +24,13 @@ $container['view'] = function ($container) {
     return $view;
 };
 
-$app->get('/', function($request, $response) {
-    return $this->view->render($response, 'home.twig');
-})->setName('home');
+$app->get('/contact', function($request, $response) {
+    return $this->view->render($response, 'contact.twig');
+});
 
-$app->get('/users', function($request, $response) {
-    $users = [
-        ['username' => 'john_snow'],
-        ['username' => 'andre_star'],
-        ['username' => 'remember_09'],
-    ];
-
-    return $this->view->render($response, 'users.twig', [
-        'users' => $users,
-    ]);
-})->setName('users.index');
+$app->post('/contact', function($request, $response) {
+    
+    die('Contact');
+})->setName('contact');
 
 $app->run();
