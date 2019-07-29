3136ba10abab70bd4a6bdefc600260aa671b40b6
kodokok <sabdo.tri.atmojo@gmail.com>
2019-07-29 19:42:48 +0700

    add twig-extension and register in app

--- index.php
+++ index.php
@@ -6,12 +6,22 @@ $app = new \Slim\App;
 
 $container = $app->getContainer();
 
-$container['greeting'] = function() {
-    return 'Hello from the container';
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/resources/views', [
+        'cache' => false
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+
+    return $view;
 };
 
 $app->get('/', function() {
-    echo $this->greeting;
+    
 });
 
 $app->run();
