489bcfc0c92cac2d70dd0550aeea3b37a3e92d3d
kodokok <sabdo.tri.atmojo@gmail.com>
2019-07-31 16:06:20 +0700

    update home view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Controllers\HomeController;
 use App\Controllers\TopicController;
 use App\Controllers\UserController;
+
+$app->get('/', HomeController::class . ':index');
 
 $app->group('/topics', function() {
     $this->get('', TopicController::class . ':index');
